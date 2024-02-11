e4635bedc5f596611c49fcf115cf069c3f5b2959
Kurrgat <mrcykj@gmail.com>
2024-02-11 13:52:09 +0300

    Task 7: Console 0.1

--- console.py
+++ console.py
@@ -3,18 +3,19 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
-
+import re
+import json
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB project."""
 
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """Exit the program at EOF."""
         print()
         return True
@@ -23,91 +24,124 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing on empty line."""
         pass
 
-    def do_create(self, arg):
-        """Create a new instance of BaseModel."""
-        if not arg:
+    def do_create(self, line):
+        """Creates an instance."""
+        if line == "" or line is None:
             print("** class name missing **")
-            return
-        try:
-            new_instance = eval(arg)()
-            new_instance.save()
-            print(new_instance.id)
-        except NameError:
+        elif line not in storage.classes():
             print("** class doesn't exist **")
+        else:
+            b = storage.classes()[line]()
+            b.save()
+            print(b.id)
 
-    def do_show(self, arg):
+    def do_show(self, line):
         """Prints the string representation of an instance."""
-        if not arg:
+        if line == "" or line is None:
             print("** class name missing **")
-            return
-        args = arg.split()
-        if args[0] not in ["BaseModel"]:
-            print("** class doesn't exist **")
-            return
-        if len(args) == 1:
-            print("** instance id missing **")
-            return
-        obj_dict = storage.all()
-        key = args[0] + "." + args[1]
-        if key in obj_dict:
-            print(obj_dict[key])
         else:
-            print("** no instance found **")
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(words) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(words[0], words[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
 
-    def do_destroy(self, arg):
+    def do_destroy(self, line):
         """Deletes an instance based on the class name and id."""
-        if not arg:
+        if line == "" or line is None:
             print("** class name missing **")
-            return
-        args = arg.split()
-        class_name = args[0]
-        if class_name not in storage.classes():
-            print("** class doesn't exist **")
-            return
-        if len(args) == 1:
-            print("** instance id missing **")
-            return
-        obj_dict = storage.all()
-        key = class_name + "." + args[1]
-        if key in obj_dict:
-            del obj_dict[key]
-            storage.save()
         else:
-            print("** no instance found **")
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(words) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(words[0], words[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[key]
+                    storage.save()
 
-    def do_all(self, arg):
+    def do_all(self, line):
         """Prints all string representation of all instances."""
-        obj_dict = storage.all()
-        if arg and arg not in ["BaseModel"]:
-            print("** class doesn't exist **")
-            return
-        print([str(obj_dict[obj]) for obj in obj_dict])
+        if line != "":
+            words = line.split(' ')
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            else:
+                nl = [str(obj) for key, obj in storage.all().items()
+                      if type(obj).__name__ == words[0]]
+                print(nl)
+        else:
+            new_list = [str(obj) for key, obj in storage.all().items()]
+            print(new_list)
 
-    def do_update(self, arg):
+    def do_count(self, line):
+        """Counts the instances of a class."""
+        words = line.split(' ')
+        if not words[0]:
+            print("** class name missing **")
+        elif words[0] not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            matches = [
+                k for k in storage.all() if k.startswith(
+                    words[0] + '.')]
+            print(len(matches))
+
+    def do_update(self, line):
         """Updates an instance based on the class name and id."""
-        if not arg:
+        if line == "" or line is None:
             print("** class name missing **")
             return
-        args = arg.split()
-        if args[0] not in ["BaseModel"]:
+
+        rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(rex, line)
+        classname = match.group(1)
+        uid = match.group(2)
+        attribute = match.group(3)
+        value = match.group(4)
+        if not match:
+            print("** class name missing **")
+        elif classname not in storage.classes():
             print("** class doesn't exist **")
-            return
-        if len(args) == 1:
+        elif uid is None:
             print("** instance id missing **")
-            return
-        key = args[0] + "." + args[1]
-        obj_dict = storage.all()
-        if key not in obj_dict:
-            print("** no instance found **")
-            return
-        if len(args) == 2:
-            print("** attribute name missing **")
-            return
-        if len(args) == 3:
-            print("** value missing **")
-            return
-        setattr(obj_dict[key], args[2], eval(args[3]))
-        obj_dict[key].save()
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            elif not attribute:
+                print("** attribute name missing **")
+            elif not value:
+                print("** value missing **")
+            else:
+                cast = None
+                if not re.search('^".*"$', value):
+                    if '.' in value:
+                        cast = float
+                    else:
+                        cast = int
+                else:
+                    value = value.replace('"', '')
+                attributes = storage.attributes()[classname]
+                if attribute in attributes:
+                    value = attributes[attribute](value)
+                elif cast:
+                    try:
+                        value = cast(value)
+                    except ValueError:
+                        pass  # fine, stay a string then
+                setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,37 +1,60 @@
 #!/usr/bin/python3
-"""Module for FileStorage class."""
+"""FileStorage Class Module.
+This module provides a class for storing and retrieving data.
+"""
+
+import datetime
 import json
-from os import path
+import os
 
 
 class FileStorage:
-    """Serializes instances to JSON file and deserializes to instances."""
+    """Class for storing and retrieving data."""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
+        """Return the dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
+        """Set in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to JSON file (path: __file_path)."""
-        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, 'w', encoding="utf-8") as file:
-            json.dump(obj_dict, file)
+        """Serialize __objects to the JSON file (path: __file_path)."""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
+
+    def classes(self):
+        """Return a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+
+        classes = {
+            "BaseModel": BaseModel,
+        }
+        return classes
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
-        if path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r', encoding="utf-8") as file:
-                obj_dict = json.load(file)
-                for key, val in obj_dict.items():
-                    cls_name, obj_id = key.split('.')
-                    cls = globals()[cls_name]
-                    obj = cls(**val)
-                    self.new(obj)
+        """Reload the stored objects."""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """Return the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel": {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime
+            }
+        }
+        return attributes
